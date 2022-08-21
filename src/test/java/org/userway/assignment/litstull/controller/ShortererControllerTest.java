d453fa50d17d6ffaaedeccda9248d07046fcc81b
Yurii (AZbest) Biliai <flaxazbest@gmail.com>
2022-08-22 02:43:58 +0300

    add empty ShortererControllerTest.java

---
+++ src/test/java/org/userway/assignment/litstull/controller/ShortererControllerTest.java
@@ -0,0 +1,16 @@
+package org.userway.assignment.litstull.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShortererControllerTest {
+
+    @Test
+    void getShortUrl() {
+    }
+
+    @Test
+    void getOriginUrl() {
+    }
+}
